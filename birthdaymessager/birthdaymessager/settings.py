0749392349e411c5b89ebae9172bb37d206bdfb9
Matt Harris <matrharr@gmail.com>
2016-07-07 16:45:17 -0700

    add redis/celery

--- birthdaymessager/birthdaymessager/settings.py
+++ birthdaymessager/birthdaymessager/settings.py
@@ -119,3 +119,10 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 
+# Celery Stuff
+BROKER_URL = 'redis://localhost:6379'
+CELERY_RESULT_BACKEND = 'redis://localhost:6379'
+CELERY_ACCEPT_RESULT = ['application/json']
+CELERY_TASK_SERIALIZER = 'json'
+CELERY_RESULT_SERIALIZER = 'json'
+CELERY_TIMEZONE = 'Africa/Nairobi'
